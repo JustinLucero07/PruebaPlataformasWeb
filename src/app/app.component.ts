4a74b90e5c550b352683e8cd1ba0fafd06ab581e
JustinLucero07 <justinluceroxd04@gmail.com>
2024-10-27 21:39:12 -0500

    simulacion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TareasComponentComponent } from './components/tareas-component/tareas-component.component';
 import { HomeComponentComponent } from './components/home-component/home-component.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,TareasComponentComponent,HomeComponentComponent],
+  imports: [RouterOutlet,TareasComponentComponent,HomeComponentComponent,RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { HomeComponentComponent } from './components/home-component/home-component.component';
+import { TareasComponentComponent } from './components/tareas-component/tareas-component.component';
+import { AppComponent } from './app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: AppComponent
+    },
+    {
+        path: 'components/tareas',
+        component: TareasComponentComponent
+    },
+    {
+        path: 'home-component',
+        component: HomeComponentComponent
+    }
+
+];
